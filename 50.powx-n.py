2112667deab80a2d0e17f6252e27dd6aaf3c2145
Rahul Sharma <rsharma842756@gmail.com>
2023-06-29 13:54:04 +0530

    Add solution for #50 in Python

---
+++ 50.powx-n.py
@@ -0,0 +1,12 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if (n == 0):
+            return 1
+
+        if (n < 0):
+            n = abs(n)
+            x = 1/x
+
+        if (n%2 == 0):
+            return self.myPow(x*x, n//2)
+        return x * self.myPow(x*x, n//2)
