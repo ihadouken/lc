542c5376dac955f021e41abdbe8a1dcb350f51e6
Rahul Sharma <rsharma842756@gmail.com>
2023-06-29 22:15:58 +0530

    Simplify conditionals for #33

--- 33.search-in-rotated-sorted-array.c
+++ 33.search-in-rotated-sorted-array.c
@@ -8,17 +8,30 @@ int search(int* nums, int numsSize, int target){
         if (nums[m] == target)
             return m;
 
-        if (nums[m] < nums[l]) {
+        // if (nums[m] < nums[l]) {
+        //     if (target <= nums[r] && target > nums[m])
+        //         l = m + 1;
+        //     else
+        //         r = m - 1;
+        // }
+        // else {
+        //     if (target >= nums[l] && target < nums[m])
+        //         r = m - 1;
+        //     else
+        //         l = m + 1;
+        // }
+
+        if (nums[m] > nums[r]) {
+            if (target > nums[r] && target < nums[m])
+                r = m - 1;
+            else
+                l = m + 1;
+        }
+        else {
             if (target <= nums[r] && target > nums[m])
                 l = m + 1;
             else
                 r = m - 1;
-        }
-        else {
-            if (target >= nums[l] && target < nums[m])
-                r = m - 1;
-            else
-                l = m + 1;
         }
     }
     return -1;
