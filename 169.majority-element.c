8b13bb29d63f1c79189fa32b156ce3d0f723fd74
Rahul Sharma <rsharma842756@gmail.com>
2023-07-16 13:40:43 +0530

    Add Boyer-Moore solution for #169 in C

--- 169.majority-element.c
+++ 169.majority-element.c
@@ -9,61 +9,89 @@
  *         the n/2 index.
  */
 
-void swap(int* x, int* y) {
-    int temp;
-    temp = *x;
-    *x = *y;
-    *y = temp;
-}
-
-int partition(int* nums, int beg, int end) {
-    int i, j;
-    for (i = j = beg; i < end; i++) {
-        if (nums[i] < nums[end]) {
-            swap(&nums[i], &nums[j]);
-            j++;
-        }
-    }
-    swap(&nums[end], &nums[j]);
-    return j;
-}
-
-// Recursive implementation.
+// void swap(int* x, int* y) {
+//     int temp;
+//     temp = *x;
+//     *x = *y;
+//     *y = temp;
+// }
+//
+// int partition(int* nums, int beg, int end) {
+//     int i, j;
+//     for (i = j = beg; i < end; i++) {
+//         if (nums[i] < nums[end]) {
+//             swap(&nums[i], &nums[j]);
+//             j++;
+//         }
+//     }
+//     swap(&nums[end], &nums[j]);
+//     return j;
+// }
+//
+// // Recursive implementation.
+// // void setMajority(int* nums, int numsSize, int beg, int end) {
+// //     int pivot = partition(nums, beg, end);
+// //
+// //     /* Found correct element to put at some index to the left of numsSize/2,
+// //      * go right to find numsSize/2. */
+// //     if (pivot < numsSize/2)
+// //         return setMajority(nums, numsSize, pivot+1, end);
+// //
+// //     /* Found correct element to put at some index to the right of numsSize/2,
+// //      * go left to find numsSize/2. */
+// //     else if (pivot > numsSize/2)
+// //         return setMajority(nums, numsSize, beg, pivot-1);
+// //
+// //     /* Implicit return when pivot = numsSize/2 */
+// // }
+//
+// // Iterative implementation.
 // void setMajority(int* nums, int numsSize, int beg, int end) {
-//     int pivot = partition(nums, beg, end);
+//     int pivot;
+//     while (beg < end) {
+//         pivot = partition(nums, beg, end);
+//         if (pivot < numsSize/2)
+//             beg = pivot + 1;
+//         else if (pivot > numsSize/2)
+//             end = pivot - 1;
+//         else
+//             break;
+//     }
+// }
 //
-//     /* Found correct element to put at some index to the left of numsSize/2,
-//      * go right to find numsSize/2. */
-//     if (pivot < numsSize/2)
-//         return setMajority(nums, numsSize, pivot+1, end);
+// int majorityElement(int* nums, int numsSize){
+//     /* Find the element (i.e. majority) placed at index numsSize/2 in a sorted version of nums.
+//      * Also put this element at numsSize/2. */
+//     setMajority(nums, numsSize, 0, numsSize-1);
 //
-//     /* Found correct element to put at some index to the right of numsSize/2,
-//      * go left to find numsSize/2. */
-//     else if (pivot > numsSize/2)
-//         return setMajority(nums, numsSize, beg, pivot-1);
-//
-//     /* Implicit return when pivot = numsSize/2 */
+//     /* This element will always be majority. */
+//     return nums[numsSize/2];
 // }
 
-// Iterative implementation.
-void setMajority(int* nums, int numsSize, int beg, int end) {
-    int pivot;
-    while (beg < end) {
-        pivot = partition(nums, beg, end);
-        if (pivot < numsSize/2)
-            beg = pivot + 1;
-        else if (pivot > numsSize/2)
-            end = pivot - 1;
-        else
-            break;
-    }
-}
-
+/* Approach: Boyer-Moore Algorithm, Complexity: O(n) in all cases */
+/* Principle: Majority has 1 more occurence then the rest of elements combined.
+ * Assume an element to be majority. If same element is encountered, we increase
+ * its count. If some other element is found, we decrement it. If the count of
+ * the majority reaches zero, it means that the rest of elements have instances
+ * equal in number than the majority. The assumption is wrong. Assume the element
+ * at this position to be majority and the count is set to one. The others will
+ * be able to cancel out n/2 instances of the majority but even then one instance
+ * higher than n/2 will let the algorithm find the majority.
+ */
 int majorityElement(int* nums, int numsSize){
-    /* Find the element (i.e. majority) placed at index numsSize/2 in a sorted version of nums.
-     * Also put this element at numsSize/2. */
-    setMajority(nums, numsSize, 0, numsSize-1);
+    int i, majority, count;
+    majority = nums[0];
+    count = 0;
+    for (i = 0; i < numsSize; ++i) {
+        if (nums[i] == majority)
+            ++count;
+        else
+            --count;
 
-    /* This element will always be majority. */
-    return nums[numsSize/2];
+        if (!count) {
+            majority = nums[i];
+            count = 1;
+        }
+    }
+    return majority;
 }
