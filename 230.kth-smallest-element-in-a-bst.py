968352fdcbc0273f29868abbaffeecacff171b7f
Rahul Sharma <rsharma842756@gmail.com>
2023-12-12 22:25:58 +0530

    Add more elegant iterative DFS solution for #230 in Python

--- 230.kth-smallest-element-in-a-bst.py
+++ 230.kth-smallest-element-in-a-bst.py
@@ -5,31 +5,58 @@
 #         self.left = left
 #         self.right = right
 
-# Approach: DFS + Stack, Complexity: O(n), O(n)
-# Tip: Inorder Traversal for BST is always sorted. Traverse till the kth node.
-
 class Solution:
+    # Approach: DFS + Stack, Complexity: O(n), O(n)
+    # Note: This is a more elegant iterative DFS solution than the one below.
+
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        # Use stack to keep track of DFS.
+        # Store nodes deferred for processing in stack.
         stack = []
-        stack.append([root, False])
+        cur = root
 
-        while stack:
-            topnode, seen_left = stack[-1]
-            # If a node has unexplored left subtree, DFS on its left child.
-            if not seen_left and topnode.left:
-                stack[-1][1] = True
-                stack.append([topnode.left, False])
+        # Continue until no nodes (untraversed or deferred) are left.
+        while cur or stack:
+            # Keep going left deferring nodes on the way.
+            while cur:
+                stack.append(cur)
+                cur = cur.left
 
-            # Else process the node and then DFS on its right child.
-            else:
-                # Count k nodes by decrement k till its zero.
-                k -= 1
-                stack.pop()
+            # When can't go further left, process a deferred node.
+            cur = stack.pop()
 
-                # Return kth node's value.
-                if k == 0:
-                    return topnode.val
+            # Return value of kth node in the inorder traversal.
+            k -= 1
+            if not k:
+                return cur.val
 
-                if topnode.right:
-                    stack.append([topnode.right, False])
+            # Do same process for the previously deferred node's right subtree.
+            cur = cur.right
+
+
+    # Approach: DFS + Stack, Complexity: O(n), O(n)
+    # Tip: Inorder Traversal for BST is always sorted. Traverse till the kth node.
+
+    # def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+    #     # Use stack to keep track of DFS.
+    #     stack = []
+    #     stack.append([root, False])
+    #
+    #     while stack:
+    #         topnode, seen_left = stack[-1]
+    #         # If a node has unexplored left subtree, DFS on its left child.
+    #         if not seen_left and topnode.left:
+    #             stack[-1][1] = True
+    #             stack.append([topnode.left, False])
+    #
+    #         # Else process the node and then DFS on its right child.
+    #         else:
+    #             # Count k nodes by decrement k till its zero.
+    #             k -= 1
+    #             stack.pop()
+    #
+    #             # Return kth node's value.
+    #             if k == 0:
+    #                 return topnode.val
+    #
+    #             if topnode.right:
+    #                 stack.append([topnode.right, False])
